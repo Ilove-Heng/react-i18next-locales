0b6a84630747667b4047d5d2ca6b44626b65d277
Nang SeakHeng <seakhengclever@gmail.com>
2024-10-30 19:14:55 +0700

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite({}), react()],
+  base: '/react-i18next-locales/',
   resolve: {
     alias: {
       "@components": "/src/components",
