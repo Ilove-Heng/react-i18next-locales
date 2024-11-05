46595e9edb74cc71cf243e24f3da24dcfdb7dce2
Nang SeakHeng <seakhengclever@gmail.com>
2024-11-05 23:02:19 +0700

    update hook

--- src/routes/$lang.ui.tsx
+++ src/routes/$lang.ui.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
-import { MacbookScroll } from '@components/ui/macbook-scroll'
 
 export const Route = createFileRoute('/$lang/ui')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <MacbookScroll src="/image/macbook-wallpaper.jpg" />
+  return 'Hello /$lang/ui!'
 }
-``
